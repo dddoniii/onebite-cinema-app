77e86f1f47f1b39e9cbd355c9018de9c3117994a
dddoniii <eunjin9487@gmail.com>
2024-09-23 21:39:34 +0900

    mission_day10(서치바 컴포넌트 만들기)

--- src/app/(with-serachbar)/layout.tsx
+++ src/app/(with-serachbar)/layout.tsx
@@ -1,44 +1,10 @@
-"use client";
-
-import style from "./layout.module.css";
-import { useRouter, useSearchParams } from "next/navigation";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
+import Searchbar from "./searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [search, setSearch] = useState("");
-
-  const q = searchParams.get("q");
-
-  useEffect(() => {
-    setSearch(q || "");
-  }, [q]);
-
-  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  const onClickSearch = () => {
-    if (!search || q === search) return;
-    router.push(`/search?q=${search}`);
-  };
-
-  const onKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") onClickSearch();
-  };
-
   return (
     <div>
-      <div className={style.searchbar_container}>
-        <input
-          value={search}
-          onChange={onChangeSearch}
-          onKeyDown={onKeydown}
-          placeholder="검색어를 입력하세요 ..."
-        />
-        <button onClick={onClickSearch}>검색</button>
-      </div>
+      <Searchbar />
       {children}
     </div>
   );
---
+++ src/app/(with-serachbar)/searchbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import style from "./searchbar.module.css";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Searchbar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClickSearch = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onClickSearch();
+  };
+
+  return (
+    <div className={style.searchbar_container}>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeydown}
+        placeholder="검색어를 입력하세요 ..."
+      />
+      <button onClick={onClickSearch}>검색</button>
+    </div>
+  );
+}
