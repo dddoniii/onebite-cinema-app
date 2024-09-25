823ac8da356e603db3c1f89a72ad724ae9fc8851
dddoniii <eunjin9487@gmail.com>
2024-09-25 22:23:08 +0900

    mission_day12(데이터 페칭 구현)

--- src/app/(with-serachbar)/page.tsx
+++ src/app/(with-serachbar)/page.tsx
@@ -1,25 +1,57 @@
 import style from "./page.module.css";
-import movies from "@/mock/dummy.json";
 import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.all_container}>
+      {allMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const recoMovies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.recommand_container}>
+      {recoMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.recommand_container}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
+        <RecoMovies />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={style.all_container}>
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
+        <AllMovies />
       </section>
     </div>
   );
